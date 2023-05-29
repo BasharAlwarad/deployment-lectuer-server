358b0fe0c07ac4814f9d925be81223e67ddb1d53
Bashar Dev <beelwarad52@gmail.com>
2023-05-29 16:00:47 +0200

    health check

--- mongoServer.js
+++ mongoServer.js
@@ -19,6 +19,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/healthz', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 // @dec     get all users from local json
 app.get('/data', async (req, res) => {
   try {
